e09b0ccb46f2e4f2396f231c83cecbccb4bdaad2
nogueirarcz <68076593+nogueirarcz@users.noreply.github.com>
2024-08-20 16:19:04 -0300

    Comentários e Docstrings em Python
    Nesta aula, aprendemos a inserir comentários e docstrings em python. Além disso, também reorganizamos a estrutura de diretórios.

---
+++ aulas/aula-02.py
@@ -0,0 +1,11 @@
+# Comentários em Python
+
+# Utilize o caractere # para criar um comentário
+# Os compentários não serão considerados pelo interpretador.
+
+'''
+    Você também pode realizar um tipo de 'comentário' utiliznado as docstrings, que servem para documentar alguma parte do código.
+    Para criar uma docstring, utilize três caracteres de aspas simples ou duplas para abrir o bloco e depois mais três para fechar.
+'''
+
+print('Comentários e docstrings')
