66384ec52d4d5789f7ffa82d597536cd04f7d065
nogueirarcz <68076593+nogueirarcz@users.noreply.github.com>
2024-08-21 20:39:44 -0300

    Tipos de dados (int e float)
    Tipos numéricos
    - Inteiros em Python fazem parte da classe int
    - Decimais em Python fazem parte da classe float

---
+++ aulas/aula-04.py
@@ -0,0 +1,12 @@
+# Tipos de dados (inteiros e decimais)
+
+# Os números inteiros em Python são da classe int
+# Já os números decimais são da classe float
+
+numero_inteiro = 10
+print(type(numero_inteiro))
+print(numero_inteiro)
+
+numero_decimal = 8.3
+print(type(numero_decimal))
+print(numero_decimal)
