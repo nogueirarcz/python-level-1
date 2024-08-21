55d1d148d957826e2dcfc2aec65920badd55aef7
nogueirarcz <68076593+nogueirarcz@users.noreply.github.com>
2024-08-21 19:14:56 -0300

    Tipos de dados (strings)
    - Conhecendo a classe str e suas características.
    
    - Utilizando a função print para exibir strings no terminal

---
+++ aulas/aula-03.py
@@ -0,0 +1,14 @@
+# Tipos de dados - Strings (str)
+
+# No Python existe uma classe própria para strings, que são um tipo de dados
+# Trata-se e uma cadeia de caracteres
+
+minha_variavel = 'Qualquer texto entre aspas será reconhecido como uma string'
+
+print(minha_variavel)
+
+# Utilizamos a função type() para verificar o tipo de dados de uma determinada variável
+print(type(minha_variavel))
+
+# A função print é utilizada para imprimir um texto no terminal
+print('A função print recebe como argumento uma variável ou uma string')
