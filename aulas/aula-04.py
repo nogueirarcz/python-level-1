3a4b1a28b17571405100ef0554c5ca1f7cd58eb9
nogueirarcz <68076593+nogueirarcz@users.noreply.github.com>
2024-08-22 07:21:10 -0300

    Incremento de comentários
    - Observações importantes.

--- aulas/aula-04.py
+++ aulas/aula-04.py
@@ -1,12 +1,16 @@
 # Tipos de dados (inteiros e decimais)
 
 # Os números inteiros em Python são da classe int
-# Já os números decimais são da classe float
-
 numero_inteiro = 10
-print(type(numero_inteiro))
+print(type(numero_inteiro)) # Utilizamos a função type() para verificar o tipo de um dado
 print(numero_inteiro)
 
+# Já os números decimais são da classe float
 numero_decimal = 8.3
-print(type(numero_decimal))
+print(type(numero_decimal)) # Utilizamos a função type() para verificar o tipo de um dado
 print(numero_decimal)
+
+'''
+Ambos os tipos (int e float) são extremamente importates para operações aritméticas e devem ser utilizados
+de acordo com o contexto do programa.
+'''
