7ea0792f6f69132c41bfea11b49337252203630f
nogueirarcz <68076593+nogueirarcz@users.noreply.github.com>
2024-08-22 07:50:04 -0300

    Observações importantes na ordem de precedência
    - Ordem de precedência dos operadores;
    - Exemplo.

--- aulas/aula-07.py
+++ aulas/aula-07.py
@@ -15,3 +15,23 @@ print(5 // 3) # Divisão inteira
 print(5 % 3) # Resto da divisão
 
 print(5 ** 3) # Potenciação
+
+'''
+É importante considerar a ordem de precedência entre os operadores, de modo semelhante ao que se faz na matemática.
+
+Em Python a ordem de precedência é a seguinte:
+()              --> parênteses                      (a partir do centro)
+**              --> exponenciação                   (da esquerda para a direita)
+* e /           --> multiplicação e divisão         (da esquerda para a direita)
++ e -           --> adição e subtração              (da esquerda para a direita)
+'''
+
+# Exemplo
+print(5**2 + ((3+2) + 1 / 2 * 5))   # primeiro o parênteses no centro (3+2) = 5
+                                    # depois dentro do parênteses da direita
+                                    # 1/2 = 0.5
+                                    # depois a multiplicação 0.5 * 5 = 2,5
+                                    # depois o que sobrou no parênteses 5 + 2,5 = 7,5
+                                    # depois a operação 5**2 = 25
+                                    # por fim a soma de 25 + 7,5 = 32,5
+  
