678d2284317177a9ce9c7b15d0d248835c6aebeb
nogueirarcz <68076593+nogueirarcz@users.noreply.github.com>
2024-08-21 21:39:07 -0300

    Operações Aritméticas
    Utilizando as operações aritméticas em Python.

---
+++ aulas/aula-07.py
@@ -0,0 +1,18 @@
+# Operadores aritméticos
+
+# É possível realizar operações aritméticas e utilizar o Python como uma espécie de calculadora.
+
+print(5 + 3) # Soma
+
+print(5 - 3) # Subtração
+
+print(5 * 3) # Multiplicação
+
+print(5 / 3) # Divisão
+
+print(5 // 3) # Divisão inteira
+
+print(5 % 3) # Resto da divisão
+
+print(5 ** 3) # Potenciação
+ 
