e04a218a17ae58e028900ee83fdda576ac5e1fd0
nogueirarcz <68076593+nogueirarcz@users.noreply.github.com>
2024-08-24 16:11:20 -0300

    Exercício 02 - Manipulando strings
    Exercício prático de manipulação e indexação de strings.

---
+++ exercicios/exercicio-02.py
@@ -0,0 +1,47 @@
+# Curso de Engenharia de Software - Algoritmos e Estruturas de Dados
+# Professor Guilherme Nogueira
+
+# Exercício 01
+
+'''
+Peça ao usuário para digitar seu nome.
+Peça ao usuário para digitar sua idade.
+
+Se nome e idade forem digitados, faça exibir:
+
+    Seu nome é {str}
+    Sua idade é {int}
+    Seu nome invertido é {str}
+    Seu nome contém ou não espaços {bool}
+    seu nome tem {int} letras
+    A primeira letra do seu nome é {str}
+    A última letra do seu nome é {str}
+
+Se nada for digitado, faça exibir:
+
+    "Para executar corretamente o programa, você deve digitar as informações. Tente novamente."
+'''
+
+# .---------------------------.
+# |         Resolução         |
+# '---------------------------'
+
+nome = input('Digite o seu nome: ')
+idade = int(input('Digite sua idade: '))
+
+if nome and idade:
+
+    print(f'Seu nome é : {nome}')
+    print(f'Sua idade é {idade}')
+    print(f'Seu nome invertido é {nome[::-1]}')
+
+    if ' ' in nome:
+        espacos = True
+    
+    else:
+        espacos = False
+
+    print(f'Seu nome contem espaços: {espacos}')
+    print(f'Seu nome tem {len(nome)} letras.')
+    print(f'A primeira letra do seu nome é {nome[0]}')
+    print(f'A última letra do seu nome é {nome[-1]}')
