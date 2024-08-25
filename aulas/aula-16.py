866903150adf673735393370b3ed2f9365141d90
nogueirarcz <68076593+nogueirarcz@users.noreply.github.com>
2024-08-25 17:13:54 -0300

    Constantes em Python
    - O que são constantes
    - Como declarar uma constante em Python

---
+++ aulas/aula-16.py
@@ -0,0 +1,22 @@
+# Constantes em Python
+
+# As contantes costumam ser valores que serão imutáveis em nossos programas e serão atribuídos a algum endereço de memória.
+# Semelhante ao que fazemos com variáveis, mas as variáveis podem ter seus valores alterados e as constantes não.
+
+'''
+Python é uma linguagem de programação conhecida por sua simplicidade, legibilidade e versatilidade. 
+Enquanto algumas linguagens têm o conceito formal de constantes, Python adota uma abordagem mais flexível. 
+
+Ao contrário de algumas linguagens que possuem palavras-chave específicas para definir constantes, 
+Python não possui um mecanismo dedicado para declarar valores constantes. No entanto, a comunidade de programadores 
+Python segue uma convenção para indicar que uma variável é considerada uma constante, utilizando letras maiúsculas 
+e separando palavras com underscores (snake_case).
+'''
+
+PI = 3.14159
+
+GRAVIDADE = 9.8
+
+NOME = 'Guilherme Nogueira'
+
+# Portanto, convencionalmente, usaremos caixa alta para identificadores de constantes.
