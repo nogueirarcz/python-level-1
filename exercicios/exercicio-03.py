263ab07f291cbd698255122215b66d09e6139cdf
nogueirarcz <68076593+nogueirarcz@users.noreply.github.com>
2024-08-26 08:31:36 -0300

    Exercício par ou ímpar
    Exercício básico, contendo estruturas condicionais e operações matemáticas

---
+++ exercicios/exercicio-03.py
@@ -0,0 +1,22 @@
+'''
+Faça um programa que peça ao usuário para digitar um número inteiro, informe se este número é par
+ou ímpar. Caso o usuário não digite um número inteiro, informe que não é um número inteiro.
+'''
+
+numero = input('Digite um número inteiro: ')
+
+if numero.isdigit():
+
+    numero = int(numero)
+
+    if (numero % 2 == 0):
+
+        print(f'O número {numero} é par.')
+    
+    else:
+
+        print(f'O número {numero} é ímpar.')
+
+else:
+
+    print('Você não informou um número inteiro. Tente novamente!')
