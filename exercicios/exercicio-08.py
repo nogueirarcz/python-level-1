1ef348398db06e1affa0d2926d8b5896bc2906c9
nogueirarcz <68076593+nogueirarcz@users.noreply.github.com>
2024-09-03 22:29:06 -0300

    Exercício de lógica
    - Listas
    - Try Exception
    - While, break
    - IF, elif, else

---
+++ exercicios/exercicio-08.py
@@ -0,0 +1,63 @@
+'''
+Faça uma lista de compras. Dê ao usuário a opção de vizualizar os itens,
+inserir e remover itens
+'''
+
+import os
+
+lista_de_compras = []
+
+while True:
+
+    print('Escolha uma opção: ')
+    opcao = input('[i]nserir - [a]pagar - [l]istar - [s]air: ')
+
+    if opcao == 'i':
+
+        os.system('cls')
+        novo_item = input('Informe o item a ser inserido: ')
+        lista_de_compras.append(novo_item)
+
+    elif opcao == 'a':
+
+        indice_do_item = input('Digite o índice para apagar: ')
+
+        try:
+
+            indice_do_item = int(indice_do_item)
+            del lista_de_compras[indice_do_item]
+
+        except TypeError:
+
+            print('Essa operação aceita apenas números inteiros! Tente novamente')
+
+        except ValueError:
+            
+            print('O índice informado não existe na lista. Tente novamente')
+
+        except Exception:
+
+            print('Operação desconhecida.')
+
+    elif opcao == 'l':
+
+        os.system('cls')
+        
+        if len(lista_de_compras) == 0:
+
+            print('Não há itens na lista.')
+
+        for indice, item in enumerate(lista_de_compras):
+
+            print(indice, item)
+
+    elif opcao == 's':
+
+        os.system('cls')
+        print('Programa encerrado.')
+        break
+
+    else:
+
+        os.system('cls')
+        print('Opção inválida! Tente novamente')
