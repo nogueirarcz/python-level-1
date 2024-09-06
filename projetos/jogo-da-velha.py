05863869014f57cdf89f73d9c1fa7bbc4c06af97
nogueirarcz <68076593+nogueirarcz@users.noreply.github.com>
2024-09-06 09:42:57 -0300

    Jogo da velha - parte 1
    Representação do tabuleiro com dicionário

---
+++ projetos/jogo-da-velha.py
@@ -0,0 +1,37 @@
+tabuleiro = {'top-l': ' ', 'top-m': ' ', 'top-r': ' ',
+    'mid-l': ' ', 'mid-m': ' ', 'mid-r': ' ',
+    'low-l': ' ', 'low-m': ' ', 'low-r': ' '             
+}
+
+def mostrar_tabuleiro(tabuleiro):
+
+    print(tabuleiro['top-l'] +' | '+ tabuleiro['top-m'] +' | '+ tabuleiro['top-r'])
+    print('- + - + -')
+
+    print(tabuleiro['mid-l'] +' | '+ tabuleiro['mid-m'] +' | '+ tabuleiro['mid-r'])
+    print('- + - + -')
+
+    print(tabuleiro['low-l'] +' | '+ tabuleiro['low-m'] +' | '+ tabuleiro['low-r'])
+
+mostrar_tabuleiro(tabuleiro)
+
+jogador = 'x'
+
+for i in range(9):
+
+    mostrar_tabuleiro(tabuleiro)
+    print(f'É a vez do jogador [{jogador}]')
+    movimento = input('Digite o seu movimento: ')
+
+    tabuleiro[movimento] = jogador
+
+    if jogador == 'x':
+
+        jogador = 'o'
+    
+    else:
+
+        jogador == 'x'
+
+    mostrar_tabuleiro(tabuleiro)
+    
