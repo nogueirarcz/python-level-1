73499963d6eeb2d675b72163c576b327e7db073d
nogueirarcz <68076593+nogueirarcz@users.noreply.github.com>
2024-09-12 08:59:46 -0300

    Validando o segundo dígito

--- exercicios/exercicio-12.py
+++ exercicios/exercicio-12.py
@@ -6,8 +6,9 @@ nove_digitos = cpf[0:9]
 contador = 10
 multiplicao_digito_contador = 0
 soma_das_multiplicacoes = 0
-digito_um = 0
 
+# Validando o primeiro dígito
+print('Validando o primeiro dígito')
 for digito in nove_digitos:
 
     print(digito, contador, ' = ', int(digito) * contador)
@@ -22,3 +23,24 @@ print(f'Soma * 10 % 11 = {digito_um}')
 
 digito_um = digito_um if digito_um <= 9 else 0
 print(f'Primeiro dígito: {digito_um}')
+
+print('\n\n')
+
+# Validando o segundo dígito
+print('Validando o segundo dígito')
+dez_digitos = nove_digitos + str(digito_um)
+contador = 11
+multiplicao_digito_contador = 0
+soma_das_multiplicacoes = 0
+
+for digito in dez_digitos:
+
+    print(digito, contador, ' = ', int(digito) * contador)
+    multiplicao_digito_contador = (int(digito) * contador)
+    soma_das_multiplicacoes += multiplicao_digito_contador
+    contador -= 1
+
+print(f'Soma: {soma_das_multiplicacoes}')
+
+digito_dois = ((soma_das_multiplicacoes * 10) % 11)
+print(f'Segundo dígito: {digito_dois}')
